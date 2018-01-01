58cf2cd682f2b71b674165c193af6c6e5789b1f4
Leelongqi <33893308+Leelongqi@users.noreply.github.com>
2018-01-01 12:18:02 +0800

    Add files via upload

--- data_process.py
+++ data_process.py
@@ -3,13 +3,13 @@ class value:
     def __init__(self,t,d):
         self.times=t
         self.date=set([d])
-with open('C:/Users/Tongji511/Desktop/distribute system/tb_call_201202_random-revise.txt', 'r') as f:
-    f3 = f.read().split('\n')
-    f3 = [i.split('\t') for i in f3]
-    f3 = random.sample(f3,k=5000)
+with open('C:/Users/75799/Desktop/1111/tb_call_201202_random.txt', 'r') as f:
+    row_data = f.read().split('\n')
+    list_data = [i.split('\t') for i in row_data]
+    part_list_data = random.sample(list_data,k=5000)
     name_dict={}
 #print(name_dict[f3[0][1]])
-    for j in f3:
+    for j in part_list_data:
         if j[1] in name_dict:
             v = name_dict[j[1]]
             v.times+=1
@@ -18,3 +18,9 @@ with open('C:/Users/Tongji511/Desktop/distribute system/tb_call_201202_random-re
         else:
             v = value(1,j[0])
             name_dict[j[1]]=v
+    with open('C:/Users/75799/Desktop/1111/tb_call_201202_random_write.txt', 'w') as f1:
+        for (k, v) in name_dict.iteritems():
+            # print(k)#user
+            date_length = len(v.date)
+            average_times = v.times / date_length
+            f1.write(k+','+str(average_times)+',' +'\n')
